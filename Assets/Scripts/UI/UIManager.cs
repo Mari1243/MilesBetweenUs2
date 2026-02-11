5986bfc3b41b363733474828f2204e6d2caa2ff8
Ann-Marie Henderson <puppy_dogs@icloud.com>
2026-02-11 15:29:27 -0500

    added check journal reminder and qr code

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -29,6 +29,7 @@ public class UIManager : MonoBehaviour
 
     private Image Hintimage;
     public GameObject LoreitemPopup;
+    private GameObject loreitemPopupVisual;
     private GameObject journalnotification;
     public static Sprite[] hintsprites;
 
@@ -105,6 +106,8 @@ public class UIManager : MonoBehaviour
                 Cursor.visible = true;
                 print("open inventory");
                 journalCanvas.enabled = true;
+
+                journalnotification.SetActive(false);
             }
             else
             {
@@ -144,7 +147,9 @@ public class UIManager : MonoBehaviour
         if(LoreitemPopup != null)
         {
             journalnotification = LoreitemPopup.transform.GetChild(0).gameObject;
+            loreitemPopupVisual = LoreitemPopup.transform.GetChild(1).gameObject;
             journalnotification.SetActive(false);
+            loreitemPopupVisual.SetActive(false);
         }
         stealingUI = StealingCanvas.transform.GetChild(1).gameObject;
         if(progressbar != null)
@@ -158,8 +163,6 @@ public class UIManager : MonoBehaviour
         if (journalCanvas != null){
             journalCanvas.enabled = false;
         }
-
-        LoreitemPopup.SetActive(false);
     }
 
     private void SetSprites()
@@ -213,11 +216,11 @@ public class UIManager : MonoBehaviour
     private IEnumerator animatejournalHud()
     {
         print("lore item is popping up");
-        LoreitemPopup.SetActive(true);
+        loreitemPopupVisual.SetActive(true);
         
         print("animating journal lore pickip indicator");
         yield return new WaitForSeconds(2.6f);
-        LoreitemPopup.SetActive(false);
+        loreitemPopupVisual.SetActive(false);
         
         //if journal is not active
         yield return new WaitForSeconds(1f);
