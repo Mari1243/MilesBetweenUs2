667f4eae72f5550517d939f5f84ff0c53a9f2ac6
Aericta <159185262+Aericta@users.noreply.github.com>
2026-02-17 22:16:47 -0500

    brother bark system work

--- Assets/Scripts/Car/BrotherInteractable.cs
+++ Assets/Scripts/Car/BrotherInteractable.cs
@@ -1,22 +1,59 @@
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BrotherInteractable : MonoBehaviour
 {
     public interactable interactableData;
     public Item newItem;
-   
+    public List<string> barks = new List<string>();
+    public int barkCount;
+    private void OnEnable()
+    {
+        DialogueManager.DialogOver += StartTimer;
+    }
+
+
     private void Start()
     {
 
+        interactableData.item.node = "Car0";
 
         if (SceneTrackerSingleton.Instance.PreviousSceneName == "GasStation")
         {
             interactableData.item = newItem;
         }
     }
+
     
+    //listens to if dialogue is not playing and when dialogue is over and executes coroutine 
     
+    //randomizes time between notification pop up
+
+    //changes interactable item data node
+
+    //pause timer
+
+    //talk UI notification appears and hovers near brother 
+
+
+
+    private void StartTimer()
+    {
+        StartCoroutine(BreakTimer());
+    }
+    private IEnumerator BreakTimer()
+    {
+        int rand=Random.Range(10, 25); //how do I space these out? Or quanitfy how many times the brother speaks to you?
+        yield return new WaitForSeconds(rand); 
+            
+        int randBark = Random.Range(0,barks.Count);
+        interactableData.item.node=barks[randBark]; //change the node in the scriptable obj 
+        //yoooo lowkey how am i getting the "barks"
+           
+    }
 
 
 
