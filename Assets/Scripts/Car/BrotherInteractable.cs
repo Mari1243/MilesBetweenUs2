d2053609c476c7eef7d940e9f2d20f3ca3a67562
Aericta <159185262+Aericta@users.noreply.github.com>
2026-02-17 22:37:39 -0500

    bark system work 2
    barks work, issues with camera when dialogue ending, bark list needs to be removed and quantified before scene ending dialogue starts.

--- Assets/Scripts/Car/BrotherInteractable.cs
+++ Assets/Scripts/Car/BrotherInteractable.cs
@@ -1,8 +1,10 @@
+using DG.Tweening;
 using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BrotherInteractable : MonoBehaviour
 {
@@ -10,17 +12,22 @@ public class BrotherInteractable : MonoBehaviour
     public Item newItem;
     public List<string> barks = new List<string>();
     public int barkCount;
+    public Image img;
     private void OnEnable()
     {
         DialogueManager.DialogOver += StartTimer;
+        DialogueManager.DialogStart += CloseBubble;
     }
-
+    private void OnDisable()
+    {
+        DialogueManager.DialogOver -= CloseBubble;
+    }
 
     private void Start()
     {
 
         interactableData.item.node = "Car0";
-
+        
         if (SceneTrackerSingleton.Instance.PreviousSceneName == "GasStation")
         {
             interactableData.item = newItem;
@@ -46,15 +53,29 @@ public class BrotherInteractable : MonoBehaviour
     }
     private IEnumerator BreakTimer()
     {
-        int rand=Random.Range(10, 25); //how do I space these out? Or quanitfy how many times the brother speaks to you?
-        yield return new WaitForSeconds(rand); 
-            
+        //problem with camera, make sure camera is in default mode before enabling this coroutine
+
+
+        //also make it so he "barks" only like twice but each time between then is randomized 
+        //ask group how we think we should approach it 
+
+        int rand =Random.Range(5, 10); //how do I space these out? Or quanitfy how many times the brother speaks to you? Also make this a public reference so you can tweak it 
+        yield return new WaitForSeconds(rand);
+
+        AnimateBubble();
         int randBark = Random.Range(0,barks.Count);
         interactableData.item.node=barks[randBark]; //change the node in the scriptable obj 
-        //yoooo lowkey how am i getting the "barks"
            
     }
 
+    private void AnimateBubble()
+    {
+        img.enabled = true;
+        img.transform.DOScale(new Vector3(1,1,1), .5f).SetEase(Ease.OutCirc);
+    }
+    private void CloseBubble()
+    {
+        img.transform.DOScale(new Vector3(0, 0, 0), .5f).SetEase(Ease.InBounce);
 
-
+    }
 }
