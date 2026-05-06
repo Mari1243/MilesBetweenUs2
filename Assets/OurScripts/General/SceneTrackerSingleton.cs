b4c4ae68de266d4776a10f91451a0dbf77bea935
Mari1243 <puppy_dogs@icloud.com>
2026-05-05 22:29:33 -0400

    badtest

--- Assets/OurScripts/General/SceneTrackerSingleton.cs
+++ Assets/OurScripts/General/SceneTrackerSingleton.cs
@@ -85,6 +85,7 @@ public class SceneTrackerSingleton : MonoBehaviour
         }
         else
         {
+            print("changing state to not car");
             NewJournalSave.instance.SetState(States.Gasstation);
             CurrentSceneEvent?.Invoke(CurrentSceneName, carnum);
             //spawn list
--- Assets/OurScripts/UI/NewJournalSave.cs
+++ Assets/OurScripts/UI/NewJournalSave.cs
@@ -61,14 +61,12 @@ public class NewJournalSave : MonoBehaviour
 
     private void OnEnable()
     {
-        InventoryManager.AddedItem += CollectedLoreItem;
         DialogueCommands.EndJournalState +=EndJournal;
         //added check
         Debug.LogError("[NJS] OnEnable: subscribed to EndJournalState.");
     }
     private void OnDisable()
     {
-        InventoryManager.AddedItem -= CollectedLoreItem;
          DialogueCommands.EndJournalState -=EndJournal;
          //added check
           Debug.LogError("[NJS] OnDisable: UNSUBSCRIBED from EndJournalState. If this fires at wrong time, journal won't respond.");
@@ -76,6 +74,7 @@ public class NewJournalSave : MonoBehaviour
 
     public void newspawnlist(GameObject data)
     {
+        print("calling newspawnlist with " +  data.name);
         if (data != null)
         {
             currentList = Instantiate(data, journal.transform);
@@ -88,10 +87,6 @@ public class NewJournalSave : MonoBehaviour
          Debug.LogError($"Prefab at index {sceneList} is null or out of range!", this);
         }
     }
-    private void CollectedLoreItem(Item item)
-    {
-        
-    }
 
     public void SetState(States newstate)
     {
@@ -100,14 +95,17 @@ public class NewJournalSave : MonoBehaviour
         if (newstate == States.Gasstation)
         {
             GasStationJournal();
+            manageItems();
         }
         else if (newstate == States.Car)
         {
             CarJournal();
+            manageItems();
         }
         else if (newstate == States.End)
         {
             EndJournalState();
+            manageItems();
         }
     }
     private void GasStationJournal()
@@ -123,8 +121,13 @@ public class NewJournalSave : MonoBehaviour
         if (currentList != null)
         {
             currentList.SetActive(true);
+            Debug.LogError("current list is " + currentList.name);
         }
-        sceneList++; // advance to next list in sequence
+        else
+        {
+            Debug.LogError("current list is null");
+        }
+            sceneList++; // advance to next list in sequence
         
     }
     //added checks
@@ -140,16 +143,13 @@ public class NewJournalSave : MonoBehaviour
         xbutton.onClick.AddListener(TriggerEndDialogue);
         Debug.LogError("[NJS] xbutton listener set to TriggerEndDialogue.");
 
-    if (inventoryObject != null)
+        if (inventoryObject != null)
         inventoryObject.SetActive(true);
-    if (currentList != null)
-    {
-        Debug.LogError("[NJS] Destroying currentList: " + currentList.name);
-        Destroy(currentList);
-    }
-
-    Debug.LogError("[NJS] Starting manage() coroutine.");
-    StartCoroutine(manage());
+        if (currentList != null)
+        {
+            Debug.LogError("[NJS] Destroying currentList: " + currentList.name);
+            Destroy(currentList);
+        }
     }
     
     //added checks
@@ -200,25 +200,14 @@ public class NewJournalSave : MonoBehaviour
             //may be causing errors???]
             Destroy(currentList);
         }
-
-        StartCoroutine(manage());
-    }
-
-    private IEnumerator manage()
-    {
-        yield return new WaitForSeconds (1f);
-        manageItems();
     }
 
     public void manageItems()
     {
         //Inventory
-        if (currentInventory.Count == 0)
+        if (currentInventory.Count != 0)
         {
-            print("Empty");
-        }
-        else
-        {
+            print("instantiating " + currentInventory.Count + " items");
             //print("instantiating inventory");
             foreach (InventoryItem items in currentInventory)
             {
@@ -231,10 +220,12 @@ public class NewJournalSave : MonoBehaviour
 
                 //this  assigns data
                 DraggableItemPrefab.GetComponent<DragItem>().itemdata=items.itemData;
-                //print("instantiating " + journalItem.name);
+                print("instantiating " + journalItem.name);
+
+                currentInventory.Remove(items);
             }
-            //added bc reinstantiation
-            currentInventory.Clear();
+
+            print("inventory is now " + currentInventory.Count + " and empty!");
         }
     }
 }
