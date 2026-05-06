218f5e6827f1c228e19cf2a06e3a7f42088538fd
Aericta <alexandraericta@gmail.com>
2026-05-06 01:15:06 -0400

    limit camera movement

--- Assets/OurScripts/Player/PlayerCam.cs
+++ Assets/OurScripts/Player/PlayerCam.cs
@@ -77,6 +77,7 @@ public class PlayerCam : MonoBehaviour
             yRotation += mouseX;
             xRotation -= mouseY;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            yRotation = Mathf.Clamp(yRotation, -90f, 90f);
 
             transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
             orientation.rotation = Quaternion.Euler(0, yRotation, 0);
